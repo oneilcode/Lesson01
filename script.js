a27c7357932995f9055c2637212d9e30c6643e78
Viktoriia Oneil <oneilcode111@gmail.com>
2022-12-02 20:59:25 +1000

    Добавления кода для задания номер два

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Урок номер 2';
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 11;
+let rollback = 50;
+let fullPrice = 100;
+let adaptive = true;
 
-console.log('Привет, я Вика');
 
-alert('Привет, нажми ок!');
+console.log(typeof (title));
+console.log(typeof (fullPrice));
+console.log(typeof (adaptive));
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " +
+   screenPrice + " рублей " + "и стоимость разработки сайта " + fullPrice + " рублей");
+
+let newScreens = screens.toLowerCase();
+console.log(newScreens.split(', '));
+
+console.log(fullPrice * rollback / 100);
