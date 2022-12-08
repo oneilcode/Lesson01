87da6feb697ae12b3ee6f2edfbb42f6e32b37515
Viktoriia Oneil <oneilcode111@gmail.com>
2022-12-08 18:00:37 +1000

    Правки

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 'use strict';
 
-let title = prompt('Как называется ваш проект?');;
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');;
-let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
 let rollback = 11;
 let adaptive = confirm('Нужен ли адаптив на сайте');
 let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Дизайн, верстка');
 let servicePrice1 = +prompt('Сколько это будет стоить?', '5000');
 let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Дизайн, верстка');
 let servicePrice2 = +prompt('Сколько это будет стоить?', '5000');
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
 
 const showTypeOf = function (variable) {
    console.log(variable, typeof variable);
+};
+
+//задание 1 (ДЗ номер 4)
+const getAllServicePrices = function () {
+   return servicePrice1 + servicePrice2;
+};
+
+//задание 2 (ДЗ номер 4) ДАННАЯ ФУНКЦИЯ ОШИБОЧНА НЕ ПОНИМАЮ ПОЧЕМУ
+function getFullPrice() {
+   return screenPrice + allServicePrices;
+}
+
+//задание 3 (ДЗ номер 4)
+
+const getTitle = function () {
+   if (!title) return title;
+   return title.trim()[0].toUpperCase() + title.trim().slice(1).toLowerCase();
+};
+
+//задание 4 (ДЗ номер 4)
+
+const getServicePercentPrices = function () {
+   return fullPrice - (fullPrice * (rollback / 100));
 };
 
 const getRollbackMessage = function (price) {
@@ -29,37 +53,10 @@ const getRollbackMessage = function (price) {
    }
 };
 
-//задание 1 (ДЗ номер 4)
-const getAllServicePrices = function () {
-   return servicePrice1 + servicePrice2;
-};
-
-let allServicePrices = getAllServicePrices;
-getAllServicePrices();
-console.log(getAllServicePrices(allServicePrices));
-
-//задание 2 (ДЗ номер 4)
-function getFullPrice(screenPrice, allServicePrices) {
-   return screenPrice + allServicePrices;
-}
-fullPrice = getFullPrice;
-getFullPrice();
-fullPrice = console.log(getFullPrice(screenPrice + allServicePrices));
-
-
-//задание 3 (ДЗ номер 4)
-
-function getTitle(title) {
-   if (!title) return title;
-   return title[0].toUpperCase() + title.slice(1).toLowerCase();
-}
-getTitle();
-console.log(getTitle(title));
-
-
-
-
-
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+title = getTitle();
+servicePercentPrice = getServicePercentPrices();
 
 showTypeOf(title);
 showTypeOf(fullPrice);
@@ -72,3 +69,5 @@ console.log(typeof (adaptive));
 
 console.log(screens.length);
 console.log(servicePercentPrice);
+
+console.log(getServicePercentPrices());
