619a577bc29b3c269421617e48860cec45bd67b0
Viktoriia Oneil <oneilcode111@gmail.com>
2022-12-08 22:05:30 +1000

    Домашнее задание номер 5

--- script.js
+++ script.js
@@ -1,41 +1,80 @@
 'use strict';
 
-let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-let screenPrice = +prompt('Сколько будет стоить данная работа?');
+let title;
+let screens;
+let screenPrice;
 let rollback = 11;
-let adaptive = confirm('Нужен ли адаптив на сайте');
-let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Дизайн, верстка');
-let servicePrice1 = +prompt('Сколько это будет стоить?', '5000');
-let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Дизайн, верстка');
-let servicePrice2 = +prompt('Сколько это будет стоить?', '5000');
-let allServicePrices;
+let adaptive;
 let fullPrice;
 let servicePercentPrice;
+let allServicePrices;
+let service1;
+let service2;
+
+
+const isNumber = function (num) {
+   return !isNaN(parseFloat(num)) && isFinite(num); //универсальная функция для проверки
+};
+
+const asking = function () {
+   title = prompt('Как называется ваш проект?', 'Проект');
+   screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+   screenPrice = prompt('Сколько будет стоить данная работа?', '10000');
+
+   do {
+      screenPrice = prompt('Сколько будет стоить данная работа?', '10000'); //задание 1 ДЗ номер 5 
+   }
+   while (!isNumber(screenPrice));
+
+
+   adaptive = confirm('Нужен ли адаптив на сайте');
+};
+
+
+const getAllServicePrices = function () {
+   let sum = 0;
+
+   for (let i = 0; i < 2; i++) {
+
+      if (i === 0) {
+         service1 = prompt('Какой дополнительный тип услуги нужен?', 'Дизайн, верстка');
+      } else if (i === 1) {
+         service2 = prompt('Какой дополнительный тип услуги нужен?', 'Дизайн, верстка');
+      }
+      sum += prompt('Сколько это будет стоить?', '5000');
+      while (!isNumber(sum)) {
+
+
+         sum = prompt('Сколько это будет стоить?', '5000'); //задание 2 ДЗ номер 5 
+      }
+   }
+
+   return sum;
+
+
+};
 
 
 const showTypeOf = function (variable) {
    console.log(variable, typeof variable);
 };
 
-//задание 1 (ДЗ номер 4)
-const getAllServicePrices = function () {
-   return servicePrice1 + servicePrice2;
-};
 
-//задание 2 (ДЗ номер 4)
+
+
+
 function getFullPrice() {
    return screenPrice + allServicePrices;
 }
 
-//задание 3 (ДЗ номер 4)
+
 
 const getTitle = function () {
    if (!title) return title;
    return title.trim()[0].toUpperCase() + title.trim().slice(1).toLowerCase();
 };
 
-//задание 4 (ДЗ номер 4)
+
 
 const getServicePercentPrices = function () {
    return fullPrice - (fullPrice * (rollback / 100));
@@ -53,6 +92,7 @@ const getRollbackMessage = function (price) {
    }
 };
 
+asking();
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
 title = getTitle();
@@ -61,6 +101,9 @@ servicePercentPrice = getServicePercentPrices();
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
+
+
+console.log('allServicePrices', allServicePrices);
 
 console.log(getRollbackMessage(fullPrice));
 console.log(typeof (title));
